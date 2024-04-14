48505fb5bc00cfcfa4e1a5cf741c3a987cf30ed1
huuth <huuthinhno1f>
2024-04-14 15:21:13 +0700

    fix image network

--- lib/src/presentation/widgets/widget_image_network.dart
+++ lib/src/presentation/widgets/widget_image_network.dart
@@ -1,9 +1,11 @@
+import 'package:base_mvvm/src/presentation/widgets/widget_shimmer.dart';
+import 'package:base_mvvm/src/presentation/widgets/widget_shimmer_container.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:octo_image/octo_image.dart';
 
 import '../../configs/configs.dart';
-import '../../utils/app_utils.dart';
+import '../../utils/utils.dart';
 
 enum ImageNetworkShape { none, circle }
 
@@ -19,14 +21,14 @@ class WidgetImageNetwork extends StatelessWidget {
 
   const WidgetImageNetwork(
       {Key? key,
-      @required this.url,
-      this.fit,
-      this.radiusAll,
-      this.height,
-      this.width,
-      this.radius,
-      this.assetError,
-      this.shape = ImageNetworkShape.none})
+        @required this.url,
+        this.fit,
+        this.radiusAll,
+        this.height,
+        this.width,
+        this.radius,
+        this.assetError,
+        this.shape = ImageNetworkShape.none})
       : super(key: key);
 
   @override
@@ -37,7 +39,13 @@ class WidgetImageNetwork extends StatelessWidget {
           image: CachedNetworkImageProvider(AppUtils.pathMediaToUrl(url ?? '')),
           height: height,
           width: width,
-          placeholderBuilder: OctoPlaceholder.blurHash('LKO2?U%2Tw=w]~RBVZRi};RPxuwH'),
+          progressIndicatorBuilder: (context, progress) => WidgetShimmer(
+            child: WidgetShimmerContainer(
+                height: height,
+                width: width,
+                radius: radiusAll,
+                borderRadius: radius ?? BorderRadius.all(Radius.circular(radiusAll ?? 0))),
+          ),
           errorBuilder: (context, error, stackTrace) =>
               Image.asset(assetError ?? AppImages.imgError, fit: BoxFit.cover),
           fit: fit ?? BoxFit.cover,
